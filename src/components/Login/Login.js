2f571ff724ba82f781709347765c09f4f17a4fe8
Muhammad Al Amin <muhammadalaminwd@gmail.com>
2022-04-09 13:53:22 +0600

    login-system-done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import useFirebase from '../hooks/useFirebase';
 
 const Login = () => {
+     const {signInWithGoogle} = useFirebase();
     return (
         <div>
             <h3>Please Login</h3>
+            <div style={{margin:"20px"}}><button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
+            <form>
+            <input type="emial" placeholder="your email"/><br></br>
+            <input type="password" name="" id="" placeholder="Password"/><br />
+            <button>Login</button>
+            </form>
         </div>
     );
 };
---
+++ src/components/Register/Register.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <form>
+            <h3>Please register now</h3>
+            <input type="emial" placeholder="your email"/><br></br>
+            <input type="password" name="" id="" placeholder="Password"/>
+            </form>
+        </div>
+    );
+};
+
+export default Register;
